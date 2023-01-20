75ad9f93cac313a41affe7fa59fdfb490a6db7fa
Scott Tai <tai199812@gmail.com>
2023-01-20 09:48:31 +0800

    added batchjob function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    let json = {};
+    json["body"] = {
+      MessageBody: "sendFromVScode",
+    };
+    async function getSQS() {
+      let res = await fetch(
+        "https://ep5ze1krj8.execute-api.ap-southeast-1.amazonaws.com/directgetfromsqs"
+      );
+      console.log(res);
+    }
+
+    async function sendSQS() {
+      let random = Math.ceil(Math.random() * 10);
+      try {
+        let res = await axios.put(
+          "https://ep5ze1krj8.execute-api.ap-southeast-1.amazonaws.com/directputtosqs",
+          { MessageBody: "send from VScode" + random }
+        );
+        console.log(res);
+      } catch (error) {
+        console.log("error" + error);
+      }
+    }
+    getSQS();
+    sendSQS();
+    setInterval(function () {
+      sendSQS();
+    }, 300000);
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
